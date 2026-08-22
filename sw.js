c0a2fd16fc62265479f2b033cf58a8dd25a2c30e
Mike Hammonds <mike.hammonds@gmail.com>
2026-08-22 12:37:04 -0400

    Refresh cache for cinematic homepage

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='china-2026-v3';
+const CACHE='china-2026-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./spectacle.css'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
